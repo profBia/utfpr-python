5dd612d956cc6e867fd5a6bfb90ed93f48be906d
Beatriz Jacinto de Almeida <106273381+profBia@users.noreply.github.com>
2025-04-01 20:11:09 -0300

    aula

---
+++ Aula 1.py
@@ -0,0 +1,5 @@
+nomeSobrenome = "Beatriz Jacinto"
+
+
+print(nomeSobrenome)
+
---
+++ numero.py
@@ -0,0 +1,14 @@
+nome = "Beatriz"       # String (texto)
+idade = 47             # inteiro (numero)
+altura = 1.54          # Float (decimal)
+estudante = True       # Booleana (Tru/False)
+
+print(nome)
+print(idade)
+print(altura)
+print(estudante)
+
+
+
+
+
